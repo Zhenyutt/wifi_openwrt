805872893e789678b4c2e1b3cd08993303d2a8c8
zhenyu <zhenyu_tien@gemteks.com>
2018-06-15 11:33:30 +0800

    delete unused files

--- ralink_ap_scan.h
+++
@@ -1,6 +0,0 @@
-#ifndef _RALINK_AP_SCAN_H
-#define _RALINK_AP_SCAN_H
-
-int ralink_ap_scan(int argc, char *argv[]);
-
-#endif //_RALINK_AP_SCAN_H
--- ralink_sta_list.h
+++
@@ -1,6 +0,0 @@
-#ifndef _RALINK_STA_LIST_H
-#define _RALINK_STA_LIST_H
-
-int ralink_sta_list(int argc, char *argv[]);
-
-#endif //_RALINK_STA_LIST_H
